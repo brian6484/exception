920d071e6071a5a3df2e382c12613a90f8e9bdf9
Brian <brianlee6484@gmail.com>
2022-06-15 13:55:56 +0900

    api exception resolver

---
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -0,0 +1,54 @@
+package hello.exception.api;
+
+import hello.exception.exception.BadRequestException;
+import hello.exception.exception.UserException;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@Slf4j
+@RestController
+public class ApiExceptionController {
+
+//    @GetMapping("/api/members/{id}")
+    public MemberDto getMember(@PathVariable("id") String id){
+        if(id.equals("ex")){
+            throw new RuntimeException("WRONG USER");
+        }
+        if(id.equals("bad")){
+            throw new IllegalArgumentException("wrong input");
+        }
+        if(id.equals("user-ex"))
+            throw new UserException("user error");
+        return new MemberDto(id, "hellooo" + id);
+    }
+
+    @GetMapping("/api/response-status-ex1")
+    public String responseStatusEx1(){
+        throw new BadRequestException();
+    }
+
+    @GetMapping("/api/response-status-ex2")
+    public String responseStatusEx2(){
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND,"error.bad", new IllegalArgumentException());
+    }
+
+    @GetMapping("/api/default-handler-ex")
+    public String defaultException(@RequestParam Integer data){
+        return "ok";
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String memberId;
+        private String name;
+    }
+}
